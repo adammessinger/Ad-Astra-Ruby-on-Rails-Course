730b4d288b3b1513afbdd39a9d4e19f6b282f46f
adammessinger <adam@adammessinger.com>
2015-05-27 16:35:35 -0500

    Add View & Controller Method for Individual Contact

--- Extended Exercises/wikipages/app/controllers/contacts_controller.rb
+++ Extended Exercises/wikipages/app/controllers/contacts_controller.rb
@@ -2,7 +2,13 @@ class ContactsController < ApplicationController
   def index
     # get data from model
     @contacts = Contact.all
-    #render view
+    # render view
+    # NOTE: We could leave next line out. By convention, Rails will automatically
+    # look for a view at [class name]/[method name].html.erb
     render('contacts/index.html.erb')
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
   end
 end
