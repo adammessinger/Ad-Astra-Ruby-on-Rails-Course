e19693c3070dc0fa1448daabb0d7fd3d8fa7d355
adammessinger <adam@adammessinger.com>
2015-05-27 17:26:55 -0500

    Update Wikipages w/ Showing & Adding of Contacts

--- Extended Exercises/wikipages/app/controllers/contacts_controller.rb
+++ Extended Exercises/wikipages/app/controllers/contacts_controller.rb
@@ -5,10 +5,31 @@ class ContactsController < ApplicationController
     # render view
     # NOTE: We could leave next line out. By convention, Rails will automatically
     # look for a view at [class name]/[method name].html.erb
-    render('contacts/index.html.erb')
+    render 'contacts/index.html.erb'
   end
 
   def show
+    # NOTE: params gives you access to query parameters
     @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(name: params[:name],
+                           email: params[:email],
+                           phone: params[:phone])
+
+    # NOTE: Jarret says that redirecting to the just-created record would be
+    # more RESTful
+    # NOTE: Calling render doesn't leave this method, so all vars available to
+    # this method are available to the rendered view.
+    if @contact.save
+      render 'contacts/success.html.erb'
+    else
+      render 'contacts/new.html.erb'
+    end
   end
 end
--- Extended Exercises/wikipages/config/routes.rb
+++ Extended Exercises/wikipages/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # route                 HTTP method     controller#method
   match('contacts', {via: :get, to: 'contacts#index'})
+  match('contacts', {via: :post, to: 'contacts#create'})
+  match('contacts/new', {via: :get, to: 'contacts#new'})
   match('contacts/:id', {via: :get, to: 'contacts#show'})
 
   # The priority is based upon order of creation: first created -> highest priority.
