808080ca50cdc53d2115c47a1847c742cdae9811
adammessinger <adam@adammessinger.com>
2015-05-27 16:18:27 -0500

    Add Route for Individual Contact

--- Extended Exercises/wikipages/config/routes.rb
+++ Extended Exercises/wikipages/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # route                 HTTP method     controller#method
-  match('contacts', {:via => :get, :to => 'contacts#index'})
+  match('contacts', {via: :get, to: 'contacts#index'})
+  match('contacts/:id', {via: :get, to: 'contacts#show'})
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
