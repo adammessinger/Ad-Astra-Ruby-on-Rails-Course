e1193b6f84ebe50b5180d61ddbbc926546909f08
adammessinger <adam@adammessinger.com>
2015-06-18 10:41:36 -0500

    Implement Basic Wikipages API
    NOTE: Destroy view removed since this is now handled with a flash
    message and a redirect.

--- Extended Exercises/wikipages/app/controllers/application_controller.rb
+++ Extended Exercises/wikipages/app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  # TEMP: disabling for easier local dev
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 end
--- Extended Exercises/wikipages/app/controllers/contacts_controller.rb
+++ Extended Exercises/wikipages/app/controllers/contacts_controller.rb
@@ -1,11 +1,20 @@
 class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
+
+    respond_to do |wants|
+      wants.html
+      wants.json {render(json: @contacts)}
+    end
   end
 
   def show
-    # NOTE: params gives you access to query parameters
     @contact = Contact.find(params[:id])
+
+    respond_to do |wants|
+      wants.html
+      wants.json {render(json: @contact)}
+    end
   end
 
   def new
@@ -13,17 +22,24 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-
-    # NOTE: Jarret says that redirecting to the just-created record would be
-    # more RESTful
     # NOTE: Calling render doesn't leave this method, so all vars available to
     # this method are available to the rendered view.
+
+    @contact = Contact.new(params[:contact])
+
     if @contact.save
-      flash[:notice] = 'Huzzah! Contact created.'
-      redirect_to @contact
+      respond_to do |wants|
+        wants.html do
+          flash[:notice] = 'Huzzah! Contact created.'
+          redirect_to @contact
+        end
+        wants.json {render(json: @contact, status: 201)}
+      end
     else
-      render 'new'
+      respond_to do |wants|
+        wants.html {render('new')}
+        wants.json {render(json: @contact.errors, status: 422)}
+      end
     end
   end
 
@@ -35,15 +51,31 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
 
     if @contact.update(params[:contact])
-      flash[:notice] = 'Boo yah! Contact created.'
-      redirect_to @contact
+      respond_to do |wants|
+        wants.html do
+          flash[:notice] = 'Boo yah! Contact updated.'
+          redirect_to @contact
+        end
+        wants.json {head :no_content}
+      end
     else
-      render 'edit'
+      respond_to do |wants|
+        wants.html {render 'edit'}
+        wants.json {render(json: @contact.errors, status: 422)}
+      end
     end
   end
 
   def destroy
     @contact = Contact.find(params[:id])
     @contact.destroy
+
+    respond_to do |wants|
+      wants.html do
+        flash[:notice]="Contact deleted."
+        redirect_to contacts_path
+      end
+      wants.json {head :no_content}
+    end
   end
 end
